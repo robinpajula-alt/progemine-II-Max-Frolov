b9206563ac225d48dee281661e2ebcc193d86fcd
Robin <robin.pajula@gmail.com>
2026-04-04 21:35:59 +0300

    Squashed 'Office-machine/' content from commit 3e81a22
    git-subtree-dir: Office-machine
    git-subtree-split: 3e81a222d13078e9ca65ed569f081fda6f6964c8

---
+++ main.py
@@ -0,0 +1,15 @@
+from devices import *
+
+if __name__ == "__main__":
+
+    printer = MyPrinter()
+    photocopier = Photocopier()
+
+    printer.print("Hello from printer")
+
+    photocopier.print("Copy this document")
+    photocopier.scan("Scanned document")
+
+    mfm = MultiFunctionMachine(printer, photocopier)
+    mfm.print("Delegated print")
+    mfm.scan("Delegated scan")
